2d3a3b95afe5015ab114112e6e8ddfa499278541
Vilem Otte <vilem.otte@post.cz>
2017-12-02 16:04:02 +0100

    Route scene

---
+++ Assets/Scripts/Route/RoutePath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoutePath : MonoBehaviour
+{
+    public GeoPoint mFrom;
+    public GeoPoint mTo;
+    public float mDistance;
+
+    /*public RoutePath(GeoPoint from, GeoPoint to)
+    {
+        mFrom = from;
+        mTo = to;
+        mDistance = GeoPoint.Distance(mFrom, mTo);
+    }*/
+
+    public void Update()
+    {
+    }
+
+    public void OnDrawGizmos()
+    {
+        mDistance = GeoPoint.Distance(mFrom, mTo);
+        
+        Gizmos.color = new Color(1.0f, 0.0f, 0.0f);
+        Gizmos.DrawLine(GeoPoint.ToCartesian(mFrom), GeoPoint.ToCartesian(mTo));
+    }
+}
