e5877515be6f4d93627b17b6f062f8b1cf17aa26
Pavel Kouřil <pk@pavelkouril.cz>
2017-12-02 21:20:27 +0100

    added day night cycle and rotation

---
+++ Assets/Daytime.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daytime : MonoBehaviour
+{
+    public const int kSecondsInDay = 60 * 60 * 24;
+
+    public const int kDaysInMonth = 30;
+
+    public const int kMonthsInYear = 12;
+
+    public const float kRealSecondsInDay = 15;
+
+    public static float DeltaTime
+    {
+        get
+        {
+            return ((Time.deltaTime / kRealSecondsInDay) * kSecondsInDay) / kSecondsInDay;
+        }
+    }
+
+    public float TimeOfDayUtc { get; set; }
+
+    public float DayOfMonth { get; set; }
+
+    public float Month { get; set; }
+
+    private void Start()
+    {
+        DayOfMonth = 1;
+        Month = 5;
+        DebugDate();
+    }
+
+    private void Update()
+    {
+        TimeOfDayUtc += DeltaTime;
+        if (TimeOfDayUtc > 1)
+        {
+            TimeOfDayUtc -= 1;
+            DayOfMonth++;
+            if (DayOfMonth > kDaysInMonth)
+            {
+                DayOfMonth = 1;
+                Month++;
+                if (Month > kMonthsInYear)
+                {
+                    Month = 1;
+                }
+            }
+            DebugDate();
+        }
+    }
+
+    private void DebugDate()
+    {
+        Debug.LogFormat("Day: {0}, Month: {1}", DayOfMonth, Month);
+    }
+}
--- Assets/Scripts/RotateGlobe.cs
+++ Assets/Scripts/RotateGlobe.cs
@@ -6,6 +6,6 @@ public class RotateGlobe : MonoBehaviour
 {
     private void Update()
     {
-        transform.Rotate(new Vector3(0, 15, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 360, 0) * Daytime.DeltaTime);
     }
 }
