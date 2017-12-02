49ee8b36de5f5dcdc1280fc963feaa7d550c18f6
Vilem Otte <vilem.otte@post.cz>
2017-12-02 22:30:29 +0100

    Plane materials & textures, and other woes

---
+++ Assets/Scripts/Plane/Engine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    public float RPM;
+
+    public void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, RPM * Time.deltaTime, Space.Self);
+    }
+}
